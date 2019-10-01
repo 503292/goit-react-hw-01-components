b2a3841bd2c4268d2d139171edf1d4f83beb6c78
Kalysh <503292@ukr.net>
2019-10-01 22:50:19 +0300

    ADD _ styles for FriendList

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ProfileComponent from './ProfileComponent/Profiles/Profiles';
 import StatsComponent from './StatsComponent/Stats/Stats';
+import FriendListComponent from './FriendListComponent/FriendList/FriendList';
+
 import stats from '../stats.json';
+import friends from '../friends.json';
 
 const App = () => {
   return (
     <>
       <ProfileComponent />
       <StatsComponent title="Upload stats" stats={stats} />
+      <FriendListComponent friends={friends} />
     </>
   );
 };
