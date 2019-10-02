9a0db70141c88452ccf0c0de9a27f7a9a05f6d60
Kalysh <503292@ukr.net>
2019-10-02 10:10:10 +0300

    Updates

---
+++ precache-manifest.436673f5f67e1abb187f4cae9baffa74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d3852a99296969839a431eb39c62f6",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "1d54e3d676d2f7962f95",
+    "url": "/goit-react-hw-01-components/static/css/main.90cb9bc8.chunk.css"
+  },
+  {
+    "revision": "804f447c63522c129d8f",
+    "url": "/goit-react-hw-01-components/static/js/2.8b23a86e.chunk.js"
+  },
+  {
+    "revision": "1d54e3d676d2f7962f95",
+    "url": "/goit-react-hw-01-components/static/js/main.efe76372.chunk.js"
+  },
+  {
+    "revision": "6e529e5db644c6f2c309",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.4f1ae2cf.js"
+  }
+]);
